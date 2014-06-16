a9d7663ae46fa6fe007351395a22a421e13586b9
jwhyte88 <jwhyte@opentable.com>
2014-06-16 16:20:56 +0100

    update

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://www.rubygems.org/"
+
+
+gem 'sinatra'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+
+group :development, :test do
+  gem "rspec"
+  gem 'database_cleaner'
+  gem 'capybara'
+end
---
+++ lib/server.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require "data_mapper"
+
+set :views, Proc.new {File.join(root, '..', "views") }
+set :public_folder, Proc.new { File.join(root, '..', "public") }
+
+env = ENV["RACK_ENV"] || "development"
+# we're telling datamapper to use a prostgres dataase on Localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link' # this needsto be done after datamapper is initialised
+
+# After declaring your models, you should finalise them
+DataMapper.finalize
+
+# However, the database tables don't excist yet. Let's tell datamapper to create them
+DataMapper.auto_upgrade! 
+
+
+get '/' do
+	@links = Link.all
+	erb: index
+end
